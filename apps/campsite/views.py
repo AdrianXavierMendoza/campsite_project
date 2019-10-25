855ecb2ff9f3d637fa62e30d76989a1c87d087fa
AdrianXavierMendoza <amen02me@sbcglobal.net>
2019-10-24 17:08:00 -0700

    5:07pm

--- apps/campsite/views.py
+++ apps/campsite/views.py
@@ -110,6 +110,7 @@ def search_results(request):
 def reservation(request, park_Id, contract_Code):
     r = requests.get("http://api.amp.active.com/camping/campground/details?parkId="+park_Id+"&contractCode="+contract_Code+"&api_key=axg5nzjhbug58fg67rfgwspc")
     obj = xmltodict.parse(r.text)
+
     lat = obj['detailDescription']['@latitude']
     lon = obj['detailDescription']['@longitude']
     w = requests.get("http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=366b23b0c77d79243f8e76a681d058eb&mode=xml")
@@ -123,12 +124,17 @@ def reservation(request, park_Id, contract_Code):
         "site_desc" : obj['detailDescription']['@description'],
         "site_img" : obj['detailDescription']['photo'][0]['@realUrl'],
         "site_other_img" :  obj['detailDescription']['photo'],
+        "site_directions" : obj['detailDescription']['@drivingDirection'],
+        "site_url" : obj['detailDescription']['@fullReservationUrl'],
         "site" : obj['detailDescription'],
+
         "site_lat" : obj['detailDescription']['@latitude'],
         "site_lon" : obj['detailDescription']['@longitude'],
+
+
         "weather_city" : weather['current']['city']['@name'],
         "weather_temp": round((float(weather['current']['temperature']['@value']) - 273.15)*(9/5)+32),
-        "weather_clouds" : weather['current']['clouds']['@name'],
+        "weather_clouds" : weather['current']['weather']['@value'],
         "weather_rain" : weather['current']['precipitation']['@mode'],
         "weather_hum": weather['current']['humidity']['@value'],
         "weather_all": weather['current'],
@@ -140,7 +146,7 @@ def reservation(request, park_Id, contract_Code):
 def confirmation(request):
     return render(request, "campsite/confirmation.html")
 
-def post_review(request, user_id, cg_id):
-    user = User.objects.get(id=request.session['id'])
-    campground = Campground.get(id=cg_id)
-    new_review = Review.objects.create(content=)
+# def post_review(request, user_id, cg_id):
+#     user = User.objects.get(id=request.session['id'])
+#     campground = Campground.get(id=cg_id)
+#     new_review = Review.objects.create(content=)
